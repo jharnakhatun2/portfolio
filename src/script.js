a391e8107c3d18dc00c81c6db7e2a0a915a79358
Jharna Khatun <jharnakhatun203@gmail.com>
2024-09-18 09:17:28 +0600

    added responsive header

--- src/script.js
+++ src/script.js
@@ -1,46 +1,17 @@
-// Tab code as sidebar
-document.addEventListener('DOMContentLoaded', ()=>{
-  const tabGroup = document.querySelectorAll(".tab");
-  tabGroup.forEach((tab)=>{
-    const buttons = tab.querySelectorAll('.tablinks');
-    const contents = document.querySelectorAll('.tabcontent');
-    
-   buttons.forEach((button)=>{
-    button.addEventListener('click', (event)=>{
-       // Retrieve menu and animationClass from data attributes
-      const targetId = button.getAttribute('data-menu');
-      const dataAnimation = button.getAttribute('data-animation');
+//  On/Off toggle button
+const openBtn = document.getElementById("open");
+const closeBtn = document.getElementById("close");
+const dropdown = document.getElementById("dropdown");
 
-      // Hide all tab content and remove animations
-      contents.forEach(content=>{
-        content.style.display = 'none';
-        content.classList.remove("slide-in-right", "slide-in-left", "slide-in-top", "slide-in-bottom");
-      });
-      
-      // Remove "active" class from all buttons
-      buttons.forEach(btn=>btn.classList.remove('active'));
-      
-      // Show the selected tab content and apply the animation class
-      const selectedId = document.getElementById(targetId);
-      selectedId.style.display='block';
-      selectedId.classList.add(dataAnimation);
-
-    // Add the "active" class to the clicked tab
-      event.currentTarget.classList.add('active');
- 
-    })
-     
-   })
-    buttons[0].click();
-  })
-  
+openBtn.addEventListener('click', function() {
+  dropdown.classList.add("open");
+  openBtn.style.display = "none";
+  closeBtn.style.display = "block";
 })
 
-//Side bar On/Off toggle button
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
-}
+closeBtn.addEventListener('click', function() {
+  dropdown.classList.remove("open");
+  openBtn.style.display = "block";
+  closeBtn.style.display = "none";
+})
 
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
