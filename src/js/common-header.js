36120f63096eb5faf5851f035a49cfda5cb9f8fa
Jharna Khatun <jharnakhatun203@gmail.com>
2024-09-22 21:23:24 +0600

    added js code update

--- src/js/common-header.js
+++ src/js/common-header.js
@@ -1,9 +1,10 @@
 // common-header.js
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("index.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("header-container").innerHTML = data;
-    })
-    .catch(error => console.error("Error loading header:", error));
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetch("header.html")
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById("header-container").innerHTML = data;
+//     })
+//     .catch(error => console.error("Error loading header:", error));
+// });
+// console.log("common header");
